39515c659bfbad04b57f68ed1219b73ed7fd63b8
Raj Kumar <raj00565@gmail.com>
2021-02-05 02:00:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,41 @@ variable "autoscale-group" {
   type        = string
   default     = "dev-env"
 }
+
+variable "autoscale-region" {
+  description = "The name to use to namespace all the resources in the cluster"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
